bbaeef6032dd5e1c7e66bd03b16e417606a2dc97
gmiller290488 <gmiller290488@gmail.com>
2018-02-01 10:21:42 +0000

    add catch for if date is past last collection on calendar to prevent infinite loop

--- bin_collection.py
+++ bin_collection.py
@@ -6,6 +6,7 @@ from datetime import datetime, timedelta
 bin_event_list = []
 unordered_date_list = []
 ordered_date_list = []
+recursion_count = 0
 
 
 def get_bin_data():
@@ -42,13 +43,15 @@ def count_the_mode_date_occurrences(date_list):
 
 
 def find_all_occurrences_of_input_date(date_input, count):
-    index = is_date_on_calendar(date_input)
+    index = is_date_on_calendar(date_input, 0)
     if date_input:
         positions_of_date_input_in_unordered_list = []
         for sublist in ordered_date_list:
             if sublist[1] == index:
                 index_of_date_input_in_ordered_list = ordered_date_list.index(sublist)
                 break
+            else:
+                return
         positions_of_date_input_in_unordered_list.append(ordered_date_list[index_of_date_input_in_ordered_list][1])
         for i in range(1, count):
             if index_of_date_input_in_ordered_list + i < len(ordered_date_list):
@@ -67,14 +70,17 @@ def is_date_valid(date):
             print('Invalid date form!  Please use the format mm/dd/YYYY')
 
 
-def is_date_on_calendar(date_input):
+def is_date_on_calendar(date_input, recursion_count):
+    recursion_count += 1
     try:
         return unordered_date_list.index(date_input)
 
     except ValueError:
+        if recursion_count > 14:
+            return print("No more collections scheduled")
         date_obj = datetime.strptime(date_input, '%m/%d/%Y')
         date_obj += timedelta(days=1)
-        return is_date_on_calendar(date_obj.strftime('%m/%d/%Y'))
+        return is_date_on_calendar(date_obj.strftime('%m/%d/%Y'), recursion_count)
 
 
 def print_results(indexs):
